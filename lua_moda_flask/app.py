d712c9ccdfe77d82c2f6df2008a0b2b9ec8053a2
Angierru <angie_ruizst@gfc.edu.co>
2026-09-11 10:41:04 -0500

    Update app.py

--- lua_moda_flask/app.py
+++ lua_moda_flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 import mysql.connector
 
 app = Flask(__name__)
-
+application = app
 
 def conectar():
     conexion = mysql.connector.connect(
